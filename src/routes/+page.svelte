072aa7e0374aa5f0f4c624f88d8d440113b02c7e
Travis Genzer <tlgenzer@gmail.com>
2024-02-24 15:33:02 -0600

    Main Page 3

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -39,8 +39,9 @@
         <input style="display:none" type="file" accept=".jpg, .jpeg, .png" on:change={(e)=>onFileSelected(e)} bind:this={fileinput} >
     </div>
     <div class="container">
-        <div></div>
-        <button >Submit</button>
+        <div>
+            <div class="pixel2">pixelated button #2<br/>with multiple lines</div>
+        </div>
     </div>
 </div>
 
@@ -99,6 +100,113 @@
         color: #283618;
         cursor: pointer;
     }
+    .container {
+        margin: 50px;
+        display: block;
+        width: 400px;
+        margin-left: auto;
+        margin-right: auto;
+        text-align: center;
+    }
+
+    .pixel2 {
+        outline: 0;
+        grid-gap: 8px;
+        align-items: center;
+        background: 0 0;
+        border: 1px solid #000;
+        border-radius: 4px;
+        cursor: pointer;
+        display: inline-flex;
+        flex-shrink: 0;
+        font-size: 16px;
+        gap: 8px;
+        justify-content: center;
+        line-height: 1.5;
+        overflow: hidden;
+        padding: 12px 16px;
+        text-decoration: none;
+        text-overflow: ellipsis;
+        transition: all .14s ease-out;
+        white-space: nowrap;
+
+        font-size: 25px;
+        color: white;
+        height: auto;
+        margin: 10px;
+        font-family: 'VT323';
+
+        position: relative;
+        display: inline-block;
+        vertical-align: top;
+        text-transform: uppercase;
+
+        cursor: pointer;
+
+        -webkit-touch-callout: none;
+        -webkit-user-select: none;
+        -khtml-user-select: none;
+        -moz-user-select: none;
+        -ms-user-select: none;
+        user-select: none;
+    }
+
+    .pixel2:active {
+        top: 2px;
+        box-shadow: 4px 4px 0 #000;
+        transform: translate(-4px,-4px);
+
+
+    }
+    :focus-visible{
+        outline-offset: 1px;
+    }
+
+    .pixel2 {
+        position: relative;
+        display: block;
+        margin: 10px;
+        font-family: 'VT323';
+        text-transform: uppercase;
+
+        font-size: 25px;
+        color: white;
+    }
+
+    .pixel2::before {
+        content: "";
+        display: block;
+        position: absolute;
+        top: 10px;
+        bottom: 10px;
+        left: -10px;
+        right: -10px;
+        background: black;
+        z-index: -1;
+    }
+
+    .pixel2::after {
+        content: "";
+        display: block;
+        position: absolute;
+        top: 4px;
+        bottom: 4px;
+        left: -6px;
+        right: -6px;
+        background: black;
+        z-index: -1;
+    }
+
+    .pixel2 {
+        padding: 10px 10px;
+        position: relative;
+        background: black;
+        width: auto;
+        z-index: 2;
+    }
+
 </style>
 
- 
+
+
+
