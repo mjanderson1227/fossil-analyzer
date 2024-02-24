f27638846b2c77111765ce8d5e01a87e3a4f3f64
Harsh <patelkharsh2@gmail.com>
2024-02-24 15:45:32 -0600

    results page

---
+++ src/routes/results/+page.svelte
@@ -0,0 +1,37 @@
+<svelte:head>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Gochi+Hand&family=Press+Start+2P&display=swap" rel="stylesheet">   
+</svelte:head>
+
+<script lang='ts'>
+    let colors = [
+		"#4F505C",
+		"#272833"
+	]
+	let degstring = '45deg'
+</script>
+
+<nav> 
+    <a href="/">home</a>
+</nav>
+<h1> Results:</h1>
+<div
+
+></div>
+
+<style>
+    :global(body){
+        height:100vh;
+        background: linear-gradient(135deg, #1F2033, #4F505C)
+    }
+    
+    
+    h1
+    {
+        font-family: 'Press Start 2P';
+        color:white
+    }
+
+
+</style>
