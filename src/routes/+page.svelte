2d5081e0eaf4b5ec20f30cfa972842b1d227b154
xxmatthewandersonxx <92887765+mjanderson1227@users.noreply.github.com>
2024-02-24 15:31:07 -0600

    Fixed TypeScript compilation errors.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,45 +1,69 @@
 <script lang="ts">
-    import logo from '$lib/img/image_photo_icon.png';
-    import file from '$lib/img/file_folder.png';
+    import logo from "$lib/img/image_photo_icon.png";
+    import file from "$lib/img/file_folder.png";
 
-    let  avatar, fileinput;
-	
-	const onFileSelected =(e)=>{
-  let image = e.target.files[0];
-            let reader = new FileReader();
-            reader.readAsDataURL(image);
-            reader.onload = e => {
-                 avatar = e.target.result
-            };
-}
+    let avatar: string, fileinput: HTMLInputElement;
+
+    const onFileSelected = (
+        e: Event & { currentTarget: EventTarget & HTMLInputElement },
+    ) => {
+        const files = (e.target as HTMLInputElement).files;
+
+        if (!files) return;
+
+        let image = files[0];
+
+        let reader = new FileReader();
+        reader.readAsDataURL(image);
+        reader.onload = (e) => {
+            const stringResult = e.target?.result?.toString();
+            if (stringResult) {
+                avatar = stringResult;
+            }
+        };
+    };
 </script>
 
-
-
-<div id="app" >
-
+<div id="app">
     <h1><strong>Get Boned</strong></h1>
-    <hr width="25%" color="bc6c25"/>
-    <h2>Upload a fossil  for analysis</h2>
+    <hr />
+    <h2>Upload a fossil for analysis</h2>
     <div id="drag_box">
-    {#if avatar}
-        <img class="avatar" src="{avatar}" alt="d" />
+        {#if avatar}
+            <img class="avatar" src={avatar} alt="d" />
         {:else}
-        <img class="avatar" src="{logo}" alt="" />
-
+            <img class="avatar" src={logo} alt="" />
         {/if}
-
-        </div>
-				<img class="upload" src={file} alt="" on:click={()=>{fileinput.click();}} />
-        <div class="chan" on:click={()=>{fileinput.click();}}>Choose Image</div>
-        <input style="display:none" type="file" accept=".jpg, .jpeg, .png" on:change={(e)=>onFileSelected(e)} bind:this={fileinput} >
-
+    </div>
+    <img
+        class="upload"
+        src={file}
+        alt=""
+        on:click={() => {
+            fileinput.click();
+        }}
+    />
+    <div
+        class="chan"
+        on:click={() => {
+            fileinput.click();
+        }}
+    >
+        Choose Image
+    </div>
+    <input
+        style="display:none"
+        type="file"
+        accept=".jpg, .jpeg, .png"
+        on:change={(e) => onFileSelected(e)}
+        bind:this={fileinput}
+    />
 </div>
 
 <style>
     :global(body) {
         height: 100vh;
-        background: linear-gradient(360deg ,#283618, #FEFAE0);
+        background: linear-gradient(360deg, #283618, #fefae0);
     }
     #drag_box {
         display: flex;
@@ -47,47 +71,47 @@
         justify-content: center;
         height: 400px;
         width: 550px;
-        border: 4px dashed #BC6C25;
+        border: 4px dashed #bc6c25;
         border-radius: 10px;
         margin: 10px;
-
-
     }
-	#app{
-	display:flex;
-		align-items:center;
-		justify-content:center;
-		flex-flow:column;
+    #app {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        flex-flow: column;
+    }
 
-}
- 
-	.upload{
-		display:flex;
-	    height:50px;
-		width:50px;
-		cursor:pointer;
-	}
-    .upload:hover{
+    #app > hr {
+        width: 25%;
+        color: "bc6c25";
+    }
+
+    .upload {
+        display: flex;
+        height: 50px;
+        width: 50px;
+        cursor: pointer;
+    }
+    .upload:hover {
         transform: scale(1.1);
     }
-	.avatar{
-		display:flex;
-		height:300px;
-		width:300px;
-	}
+    .avatar {
+        display: flex;
+        height: 300px;
+        width: 300px;
+    }
     h1 {
-        color: #BC6C25;
+        color: #bc6c25;
     }
     h2 {
         color: #283618;
     }
     .chan {
-        color: #BC6C25;
+        color: #bc6c25;
     }
     .chan:hover {
         color: #283618;
         cursor: pointer;
     }
 </style>
-
- 
