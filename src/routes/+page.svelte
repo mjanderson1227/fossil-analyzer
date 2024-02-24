8abca6348380a3141e702ae45d7759a12f0dd178
Travis Genzer <tlgenzer@gmail.com>
2024-02-24 15:15:33 -0600

    Main Page 2

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,14 @@
+<svelte:head>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Gochi+Hand&family=Press+Start+2P&display=swap" rel="stylesheet">
+</svelte:head>
 <script lang="ts">
     import logo from '$lib/img/image_photo_icon.png';
     import file from '$lib/img/file_folder.png';
 
-    let  avatar, fileinput;
+    let avatar: string | ArrayBuffer | null;
+    let fileinput: string | undefined;
 	
 	const onFileSelected =(e)=>{
   let image = e.target.files[0];
@@ -18,22 +24,24 @@
 
 <div id="app" >
 
-    <h1><strong>Get Boned</strong></h1>
+    <h1><strong>What the Dino?!</strong></h1>
     <hr width="25%" color="bc6c25"/>
-    <h2>Upload a fossil  for analysis</h2>
-    <div id="drag_box">
+<!--    <h2>Upload a fossil  for analysis</h2>-->
+    <div id="drag_box" on:click={()=>{fileinput.click();}}>
     {#if avatar}
         <img class="avatar" src="{avatar}" alt="d" />
         {:else}
         <img class="avatar" src="{logo}" alt="" />
-
         {/if}
 
-        </div>
-				<img class="upload" src={file} alt="" on:click={()=>{fileinput.click();}} />
-        <div class="chan" on:click={()=>{fileinput.click();}}>Choose Image</div>
+<!--				<img class="upload" src={file} alt="" on:click={()=>{fileinput.click();}} />-->
+<!--        <div class="chan" on:click={()=>{fileinput.click();}}>Choose Image</div>-->
         <input style="display:none" type="file" accept=".jpg, .jpeg, .png" on:change={(e)=>onFileSelected(e)} bind:this={fileinput} >
-
+    </div>
+    <div class="container">
+        <div></div>
+        <button >Submit</button>
+    </div>
 </div>
 
 <style>
@@ -45,7 +53,7 @@
         display: flex;
         align-items: center;
         justify-content: center;
-        height: 400px;
+        height: 300px;
         width: 550px;
         border: 4px dashed #BC6C25;
         border-radius: 10px;
@@ -77,12 +85,15 @@
 	}
     h1 {
         color: #BC6C25;
+        font-family: 'Press Start 2P';
     }
     h2 {
         color: #283618;
+        font-family: 'Press Start 2P';
     }
     .chan {
         color: #BC6C25;
+        font-family: 'Press Start 2P';
     }
     .chan:hover {
         color: #283618;
